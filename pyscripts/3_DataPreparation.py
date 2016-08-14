6ed80ca96d832c6e83b1d5486b4107c3ee0332a5
Emmett McQuinn <shakencup@yahoo.com>
2016-08-14 10:08:55 -0700

    Updated to use master's changes.

--- pyscripts/3_DataPreparation.py
+++ pyscripts/3_DataPreparation.py
@@ -4,19 +4,18 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    team = pd.read_csv('../data/Max_Animals_TEAM_data.csv', index_col=0)
+    team = pd.read_csv('../data/intermediate/Max_Animals_TEAM_data.csv', index_col=0)
     team['Sampling Period'] = team['Sampling Period'].apply(lambda x: str(x).replace('.', '-'))
     team['Year'] = team['Sampling Period'].apply(lambda x: x[0:4])
 
-    # NOTE(emmett): I copied Terrestrial_Vertabrate_Cols_Edited.csv to this file.   
-    lat_long = pd.read_csv('../data/Terrestrial_Vertebrate.csv', index_col=0)
+    lat_long = pd.read_csv('../data/original/Terrestrial_Vertebrate.csv', index_col=0)
     lat_long = lat_long[['Sampling Unit Name', 'Latitude', 'Longitude']]
     lat_long = lat_long.drop_duplicates('Sampling Unit Name').reset_index()
-    
+
     team = pd.merge(team, lat_long, on=['Sampling Unit Name'], how='left')
     team['Sampling Type'] = 'Annual'
     team['Data Source'] = 'TEAM'
-    
+
     rate = pd.read_csv('../data/team_trap_days.csv')
     rate['Sampling.Period'] = rate['Sampling.Period'].apply(lambda x: str(x).replace('.', '-'))
 
@@ -27,4 +26,4 @@ if __name__ == '__main__':
     del team['trap_days_per_100']
     del team['trap_days']
 
-    team.to_csv('rate_of_detection.csv', index=False)
+    team.to_csv('../data/preprocessed/rate_of_detection.csv', index=False)
