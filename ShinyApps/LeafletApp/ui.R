4a195c1ffad95777c0e9ea16dc4f3ea5356b1d16
pavlovc2 <pavlovc2@illinois.edu>
2016-08-14 02:01:19 -0700

    Some new stuff

--- ShinyApps/LeafletApp/ui.R
+++ ShinyApps/LeafletApp/ui.R
@@ -1,3 +1,4 @@
+library(raster)
 library(shiny)
 library(leaflet)
 
@@ -35,44 +36,45 @@ shinyUI(navbarPage("Rates of detection", id="nav",
 
       # Shiny versions prior to 0.11 should use class="modal" instead.
       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = 10,
+        draggable = FALSE, top = 60, left = "auto", right = 20, bottom = 10,
         width = 330, height = "auto", style = "overflow-y:scroll",
 
         h2("Rates of detection"),
-	selectInput("dataset", "Camera Trap Project", c("TEST!")),
-        uiOutput("site_checkbox"),
-	
-	
-	#####     SLIDER 
-	selectInput("select_time", label = "Sampling Frequency", 
-	            choices = list("Annual" = 'annual', "Seasonal" = 'seasonal', "Monthly" = 'monthly'), 
-	            selected = 1),
-	
-	sliderInput("time_slider", label = "Select Time", min = 0, 
-	            max = 100, value = 50, timeFormat = "%Y-%m-%d"),
-	
-	#####
-	
-	
-        radioButtons("humans", "Show Humans?", c("Humans", "No Humans"), 
-                    selected="No Humans"),
+      	selectInput("dataset", "Camera Trap Project", c("TEST!")),
+              uiOutput("site_checkbox"),
+      	
+      	
+      	#####     SLIDER 
+
+      #   # TODO: Make this reactive based on frequencies present in input data
+      # 	selectInput("select_time", label = "Sampling Frequency", 
+      # 	            choices = list("Annual" = 'annual', "Seasonal" = 'seasonal', "Monthly" = 'monthly'), 
+      # 	            selected = 1),
+      # 	
+      #   # TODO: Make this into a reactive UI based on min() and max() of actual data for sites.
+      # 	sliderInput("time_slider", label = "Select Time", min = 0, 
+      # 	            max = 100, value = 50, timeFormat = "%Y-%m-%d"),
+        #radioButtons("humans", "Show Humans?", c("Humans", "No Humans"), 
+        #            selected="No Humans"),
         uiOutput("guild.control"),
         uiOutput("red.control"),
         uiOutput("species.list"),
-        uiOutput("time.selection"),
-        selectInput("color", "Color", vars),
-        selectInput("size", "Size", vars, selected = "adultpop"),
-        conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
-                         # Only prompt for threshold when coloring or sizing by superzip
-                         numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
-        ),
-        selectInput(inputId = "samplingFrequency",
-                    label = "Sampling Frequency",
-                    choices = samplingFrequency),
-        checkboxInput(inputId = "show_human",
-                      label = "Show Human Activities?"),
-        plotOutput("histCentile", height = 200),
-        plotOutput("scatterCollegeIncome", height = 250)
+        uiOutput("frequency.control"),
+        uiOutput("time.control")
+        #uiOutput("time.selection")#,
+        # selectInput("color", "Color", vars),
+        # selectInput("size", "Size", vars, selected = "adultpop"),
+        # conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+        #                  # Only prompt for threshold when coloring or sizing by superzip
+        #                  numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+        # ),
+        # selectInput(inputId = "samplingFrequency",
+        #             label = "Sampling Frequency",
+        #             choices = samplingFrequency),
+        # checkboxInput(inputId = "show_human",
+        #               label = "Show Human Activities?"),
+        # plotOutput("histCentile", height = 200),
+        # plotOutput("scatterCollegeIncome", height = 250)
       ),
 
       tags$div(id="cite",
