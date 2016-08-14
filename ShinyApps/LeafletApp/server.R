31e8b68e6f2b6124819bca144b7cba5a1b786286
erincr <erinc@Erins-MacBook-Pro.local>
2016-08-13 21:49:23 -0700

    Reorganize/remove old code

--- ShinyApps/LeafletApp/server.R
+++ ShinyApps/LeafletApp/server.R
@@ -24,193 +24,6 @@ red.list.table <- read.csv("data/taxonomy_red_list_status_20160813.csv")
 red.list.table <- subset(red.list.table, id %in% c(3,4,8,9,5))
 
 shinyServer(function(input, output, session) {
-
-  ## Interactive Map ###########################################
-
-  # Get unique pairs of lat long values for plotting cam locations
-  locs <- select(lat_long_data, Latitude, Longitude)
-  cam_lat_longs <- unique(locs)
-  
-  # Create the map
-  output$map <- renderLeaflet({
-    dat <- get_KDE_polygons(site_selection())
-    leaflet(cam_lat_longs) %>% 
-      addTiles(
-        urlTemplate = "http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}"
-      ) %>% 
-      addCircleMarkers(~Longitude, ~Latitude, weight=2, radius=2, color="black", fillOpacity=1, layerId=NULL) %>% 
-      addPolygons(data=dat$poly, color = brewer.pal(dat$nlev, "Greens")[dat$levs], stroke=FALSE)
-  
-  })
-  
-
-
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  zipsInBounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(zipdata[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
-
-    subset(zipdata,
-      latitude >= latRng[1] & latitude <= latRng[2] &
-        longitude >= lngRng[1] & longitude <= lngRng[2])
-  })
-
-  # Precalculate the breaks we'll need for the two histograms
-  centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
-
-  output$histCentile <- renderPlot({
-    # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
-      return(NULL)
-
-    hist(zipsInBounds()$centile,
-      breaks = centileBreaks,
-      main = "SuperZIP score (visible zips)",
-      xlab = "Percentile",
-      xlim = range(allzips$centile),
-      col = '#00DD00',
-      border = 'white')
-  })
-
-  output$scatterCollegeIncome <- renderPlot({
-    # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
-      return(NULL)
-
-    print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  })
-
-  # This observer is responsible for maintaining the circles and legend,
-  # according to the variables the user has chosen to map to color and size.
-#   observe({
-#     colorBy <- input$color
-#     sizeBy <- input$size
-# 
-#     if (colorBy == "superzip") {
-#       # Color and palette are treated specially in the "superzip" case, because
-#       # the values are categorical instead of continuous.
-#       colorData <- ifelse(zipdata$centile >= (100 - input$threshold), "yes", "no")
-#       pal <- colorFactor("Spectral", colorData)
-#     } else {
-#       colorData <- zipdata[[colorBy]]
-#       pal <- colorBin("Spectral", colorData, 7, pretty = FALSE)
-#     }
-# 
-#     if (sizeBy == "superzip") {
-#       # Radius is treated specially in the "superzip" case.
-#       radius <- ifelse(zipdata$centile >= (100 - input$threshold), 30000, 3000)
-#     } else {
-#       radius <- zipdata[[sizeBy]] / max(zipdata[[sizeBy]]) * 30000
-#     }
-# 
-#     leafletProxy("map", data = zipdata) %>%
-#       clearShapes() %>%
-#       addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-#         stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-#       addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-#         layerId="colorLegend")
-#   })
-
-  # Show a popup at the given location
-  showZipcodePopup <- function(zipcode, lat, lng) {
-    selectedZip <- allzips[allzips$zipcode == zipcode,]
-    content <- as.character(tagList(
-      tags$h4("Score:", as.integer(selectedZip$centile)),
-      tags$strong(HTML(sprintf("%s, %s %s",
-        selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
-      ))), tags$br(),
-      sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
-      sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
-      sprintf("Adult population: %s", selectedZip$adultpop)
-    ))
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
-  }
-
-  # When map is clicked, show a popup with city info
-  observe({
-    leafletProxy("map") %>% clearPopups()
-    event <- input$map_shape_click
-    if (is.null(event))
-      return()
-
-    isolate({
-      showZipcodePopup(event$id, event$lat, event$lng)
-    })
-  })
-
-
-  ## Data Explorer ###########################################
-
-  observe({
-    cities <- if (is.null(input$states)) character(0) else {
-      filter(cleantable, State %in% input$states) %>%
-        `$`('City') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$cities[input$cities %in% cities])
-    updateSelectInput(session, "cities", choices = cities,
-      selected = stillSelected)
-  })
-
-  observe({
-    zipcodes <- if (is.null(input$states)) character(0) else {
-      cleantable %>%
-        filter(State %in% input$states,
-          is.null(input$cities) | City %in% input$cities) %>%
-        `$`('Zipcode') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
-    updateSelectInput(session, "zipcodes", choices = zipcodes,
-      selected = stillSelected)
-  })
-
-  observe({
-    if (is.null(input$goto))
-      return()
-    isolate({
-      map <- leafletProxy("map")
-      map %>% clearPopups()
-      dist <- 0.5
-      zip <- input$goto$zip
-      lat <- input$goto$lat
-      lng <- input$goto$lng
-      showZipcodePopup(zip, lat, lng)
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-    })
-  })
-  
-  # TODO use filtered data as input
-  #subsettedData <- select(TEAM_data,
-  #  'Project'=Project.ID, 'Deployment Location ID'=Deployment.Location.ID,
-  #  'Latitude'=Latitude.Resolution, 'Longitude'=Longitude.Resolution,
-  #  'Sampling Type'=Sampling.Type, 'Sampling Period'=Sampling.Period, 'Year'=Year,
-  #  'Genus'=Genus, 'Species'=Species, 'Rate Of Detection'=Rate.Of.Detection
-  #)
-
-  # TODO change file name based on filters
-  output$downloadData <- downloadHandler(
-    filename = function() { paste('data', '.csv', sep='') },
-    content = function(file) {
-      write.csv(subsettedData, file)
-    }
-  )
-  
-  output$table <- DT::renderDataTable({
-    df <- subsettedData %>%
-      mutate(Go = paste('<a class="go-map" href="" data-lat="', Latitude, '" data-long="', Longitude, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
-    action <- DT::dataTableAjax(session, df)
-
-    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
-  })
-  
-  # Generate controls
   
   # Read in input data based on project
   dataset_input <- reactive({
@@ -218,8 +31,6 @@ shinyServer(function(input, output, session) {
       read.csv("./data/rate_of_detection.csv")
     } 
   })
-  
-  # Add 
   
   # Select Region
   output$site_checkbox <- renderUI({
@@ -267,6 +78,64 @@ shinyServer(function(input, output, session) {
     selectInput("species", "Select Species (Multiple Possible)", 
                 choices=sort(present.species.names()), selected=NULL, multiple=TRUE)
   })
+  
+
+  ## Interactive Map ###########################################
+
+  # Get unique pairs of lat long values for plotting cam locations
+  locs <- select(lat_long_data, Latitude, Longitude)
+  cam_lat_longs <- unique(locs)
+  
+  # Create the map
+  output$map <- renderLeaflet({
+    dat <- get_KDE_polygons(site_selection())
+    leaflet(cam_lat_longs) %>% 
+      addTiles(
+        urlTemplate = "http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}"
+      ) %>% 
+      addCircleMarkers(~Longitude, ~Latitude, weight=2, radius=2, color="black", fillOpacity=1, layerId=NULL) %>% 
+      addPolygons(data=dat$poly, color = brewer.pal(dat$nlev, "Greens")[dat$levs], stroke=FALSE)
+  
+  })
+  
+  # TODO use filtered data as input
+  #subsettedData <- select(TEAM_data,
+  #  'Project'=Project.ID, 'Deployment Location ID'=Deployment.Location.ID,
+  #  'Latitude'=Latitude.Resolution, 'Longitude'=Longitude.Resolution,
+  #  'Sampling Type'=Sampling.Type, 'Sampling Period'=Sampling.Period, 'Year'=Year,
+  #  'Genus'=Genus, 'Species'=Species, 'Rate Of Detection'=Rate.Of.Detection
+  #)
+
+  # TODO change file name based on filters
+  output$downloadData <- downloadHandler(
+    filename = function() { paste('data', '.csv', sep='') },
+    content = function(file) {
+      write.csv(subsettedData, file)
+    }
+  )
+  
+  output$table <- DT::renderDataTable({
+    df <- subsettedData %>%
+      mutate(Go = paste('<a class="go-map" href="" data-lat="', Latitude, '" data-long="', Longitude, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+    action <- DT::dataTableAjax(session, df)
+
+    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+  })
+  
+  # Generate controls
+  
+  # Read in input data based on project
+  #dataset_input <- reactive({
+  #  if (input$dataset=="TEST!") {
+  #    read.csv("./data/rate_of_detection.csv")
+  #  } 
+  #})
+  
+  # Add 
+  
+ 
+  
+  #
   
   # Update species selection based on RED and guild
   observe({
