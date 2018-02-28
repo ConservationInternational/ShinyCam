f56cdbc946b3d52ac8a3f09b6d5ba1acd8a61051
Kari Norman <kari@normanfamily.org>
2018-02-28 12:04:21 -0800

    First commit.

---
+++ rscripts/species_occurence.R
@@ -0,0 +1,19 @@
+#####################
+# species_occurence.R
+# Aggregate occurences across time for each camera/location
+# The count is a crude number that doesn't account for the number of individuals in a picture
+
+library(lubridate)
+library(dplyr)
+library(plyr)
+
+read_csv("~/Documents/Projects/ShinyCam/ShinyApps/LeafletApp/data/raw_dataprep/marin_data.csv") %>%
+  select(Project.ID,Camera.Deployment.Begin.Date,Camera.Deployment.End.Date,
+         Latitude.Resolution, Longitude.Resolution,Photo.Type,Image.ID,Genus.Species,
+         Count,Deployment.Location.ID,Event,Date_Time.Captured) %>%
+  filter(Photo.Type == "Animal") -> data_marin
+
+data_marin %>%
+  select(Project.ID, Latitude.Resolution, Longitude.Resolution, Genus.Species, Deployment.Location.ID) %>%
+  add_count(Project.ID, Latitude.Resolution, Longitude.Resolution, Genus.Species, Deployment.Location.ID) %>%
+  unique() -> data_new
