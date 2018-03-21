b1f3f141aac0b1c70d15e3f2ca123351bbc6b847
cmt2 <cmt2@berkeley.edu>
2018-03-21 13:01:04 -0700

    specify dplyr version of summarise function in species_occurence script

--- rscripts/species_occurence.R
+++ rscripts/species_occurence.R
@@ -12,7 +12,7 @@ data <- read_csv("~/Documents/Projects/ShinyCam/ShinyApps/LeafletApp/data/raw_da
 data %>%
   select(Project.ID, Genus.Species, Deployment.Location.ID, total) %>%
   group_by(Project.ID, Genus.Species, Deployment.Location.ID) %>%
-  summarise(event_total = n(), individual_total = sum(total)) %>%
+  dplyr::summarise(event_total = n(), individual_total = sum(total)) %>%
   inner_join(., select(data, Deployment.Location.ID, Latitude.Resolution, Longitude.Resolution), by = "Deployment.Location.ID") %>%
   unique() -> data_new
 
