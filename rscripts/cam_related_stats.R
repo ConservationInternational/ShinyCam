19f2f6f90b22ed7418a7804054d7f4e885b0e9c1
efegraus@conservation.org <efegraus@conservation.org>
2018-07-20 16:12:57 -0700

    setting setwd(), doing a little cleaning on teh ddply functions and changing output pathe to the processed dir

--- rscripts/cam_related_stats.R
+++ rscripts/cam_related_stats.R
@@ -1,5 +1,7 @@
 ####
 # cam_related_stats.R
+# EHF: We need to review all of these and have cleaner operational statistics.
+#
 # This file includes the following summmary stats:
 # All stats are by project ID and camera ID (deployment location ID)
 # 01_count_images: Count of images
@@ -9,46 +11,49 @@
 # 05_count_wildlife: Count of wildlife images (see excluded species list)
 # 06_count_human_related: Count of human-related images (uses excluded species list)
 # 07_avg_photos_per_deployment: Average # of images per camera deployment
-
+###############################
 rm(list = ls())
-
 library(dplyr)
 library(data.table)
+# Set the path and workspace  to to main ShinyCam directory (i.e. the one that has README.md file, ShinyApps directory,etc)
+shinycam_path <- "/Users/efegraus/Documents/GitHub/ShinyCam/"
+prj_name<- "Marin" # No spaces in names
+setwd(shinycam_path)
+source("rscripts/RShiny_functions.R")
+# Set the file name and path to your raw data files. This should be a file name that is in the correct format. See README process for explanation
+#Load Data
+#df_name <- "YOUR FILENAME HERE"
+#Exmample
+df_name <- "marin_data_animals.csv" # 
+# Set the path to your local camera trap data file in the raw_dataprep ditectory
+# ct_data <- read.csv(paste("YOUR LOCALPATH TO THE REAW DATA FILE",df_name,sep=""))
+# Example
+ct_data <-fread(paste("ShinyApps/LeafletApp/data/raw_dataprep/",df_name, sep=""))
+old <- Sys.time()
 
-setwd("/Users/saksman/ShinyCam/rscripts/")
-
-dm <-  fread("../data/original/marin_data.csv", header = TRUE, sep = ',',
-  data.table = FALSE)
-
+output_path <-paste(shinycam_path,"ShinyApps/LeafletApp/data/processed/",sep="")
+###############################
 # 01: Count of images [Number of images per camera in each location]
+dm_01_count_images<- ct_data %>% group_by(Project.ID,Camera.ID,Deployment.Location.ID) %>% summarize(count_images = n())
 
-dm_01_count_images <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
-  summarize(count_images = n_distinct(Image.ID))
-
-write.csv(dm_01_count_images, "../data/processed/dm_01_count_images.csv", row.names = FALSE)
+write.csv(dm_01_count_images, file=paste(output_path,"dm_01_count_images.csv",sep=""), row.names = FALSE)
 
 # 02: Count of blanks per camera trap
+dm_02_count_blanks<- ct_data %>% group_by(Project.ID,Camera.ID,Deployment.Location.ID) %>% summarize(count_blanks = sum(Photo.Type == "Blanks"))
 
-dm_02_count_blanks <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
-  summarize(count_blanks = n_distinct(Image.ID[Photo.Type == "Blank"]))
-
-write.csv(dm_02_count_blanks, "../data/processed/dm_02_count_blanks.csv", row.names = FALSE)
+write.csv(dm_02_count_blanks, file=paste(output_path,"dm_02_count_blanks.csv",sep=""), row.names = FALSE)
 
 # 03: Count of unknown images
+dm_03_count_unknowns <- ct_data %>% group_by(Project.ID,Camera.ID,Deployment.Location.ID) %>% summarize(count_unknowns = sum(Photo.Type == "Unknown"))
 
-dm_03_count_unknowns <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
-  summarize(count_unknowns = n_distinct(Image.ID[Photo.Type == "Unknown"]))
-
-write.csv(dm_03_count_unknowns, "../data/processed/dm_03_count_unknowns.csv", row.names = FALSE)
+write.csv(dm_03_count_unknowns, file=paste(output_path,"dm_03_count_unknowns.csv",sep=""), row.names = FALSE)
 
 # 04: Count of uncatalogued images
+dm_04_count_uncatalogued  <- ct_data %>% group_by(Project.ID,Camera.ID,Deployment.Location.ID) %>% summarize(count_uncatalogued = sum(Photo.Type == ""))
 
-dm_04_count_uncatalogued <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
-  summarize(count_uncatalogued = n_distinct(Image.ID[Photo.Type == ""]))
+write.csv(dm_04_count_uncatalogued, file=paste(output_path,"dm_04_count_uncatalogued.csv",sep=""), row.names = FALSE)
 
-write.csv(dm_04_count_uncatalogued, "../data/processed/dm_04_count_uncatalogued.csv", row.names = FALSE)
-
-# 05: Count of animal images (not humans)
+# 05: Count of animal images (not humans) # EHF: THis needs to be reevaluated and clearly communicated what is being filtered here.
 # List Genus.Species to ignore:
 # Homo sapiens%
 # Canis familiaris%
@@ -57,35 +62,33 @@ write.csv(dm_04_count_uncatalogued, "../data/processed/dm_04_count_uncatalogued.
 # Equus caballus-ferus
 
 pattern <- c('Homo sapiens|Canis familiaris|Felis silvestris|Bos taurus|Equus caballus-ferus')
-ind <-  which(dm$Genus.Species %like% pattern)
-species <- unique(dm$Genus.Species[ind])
+ind <-  which(ct_data$Genus.Species %like% pattern)
+species <- unique(ct_data$Genus.Species[ind])
 
 # excluded species list (for reference)
 # "Bos taurus"                        "Homo sapiens-tourist"              "Canis familiaris"
 # "Canis familiaris-leash"            "Homo sapiens-bicyclist"            "Homo sapiens-resident"
 # "Homo sapiens-official-vehicle"     "Equus caballus-ferus"              "Homo sapiens-non-official-vehicle"
 # "Felis silvestris"                  "Homo sapiens"
+dm_05_count_wildlife <- ct_data %>% group_by(Project.ID,Camera.ID,Deployment.Location.ID) %>% 
+  summarize(count_wildlife = sum(Photo.Type == "Animal" & !(Genus.Species %in% species)))
 
-dm_05_count_wildlife <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
-  summarize(count_wildlife = n_distinct(Image.ID[Photo.Type == "Animal"
-  & !(Genus.Species %in% species)]))
-
-write.csv(dm_05_count_wildlife, "../data/processed/dm_05_count_wildlife.csv", row.names = FALSE)
+write.csv(dm_05_count_wildlife,file=paste(output_path,"dm_05_count_wildlife.csv",sep=""), row.names = FALSE)
 
 # 06: Count of human-related images
 
-dm_06_count_human_related <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
+dm_06_count_human_related <- group_by(ct_data, Project.ID, Camera.ID, Deployment.Location.ID) %>%
   summarize(count_human_related = n_distinct(Image.ID[Photo.Type == "Animal"
   & (Genus.Species %in% species)]))
 
-write.csv(dm_06_count_human_related, "../data/processed/dm_06_count_human_related.csv", row.names = FALSE)
+write.csv(dm_06_count_human_related, file=paste(output_path,"dm_06_count_human_related.csv",sep=""), row.names = FALSE)
 
 # 07: Average # of photos per camera deployment
 
-dm_07_avg_photos_per_deployment <- group_by(dm, Project.ID, Camera.ID, Deployment.Location.ID) %>%
+dm_07_avg_photos_per_deployment <- group_by(ct_data, Project.ID, Camera.ID, Deployment.Location.ID) %>%
   summarize(num_deployments = n_distinct(Deployment.ID),
   count_photos = n_distinct(Image.ID)) %>%
   mutate(avg_photos_per_deployment = round(count_photos / num_deployments, 2))
 
-write.csv(dm_07_avg_photos_per_deployment, "../data/processed/dm_07_avg_photos_per_deployment.csv", row.names = FALSE)
+write.csv(dm_07_avg_photos_per_deployment, file=paste(output_path,"dm_07_avg_photos_per_deployment.csv",sep=""), row.names = FALSE)
 
