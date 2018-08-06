9dd53391e3251010577033296ad1ef5b2490d26d
efegraus@conservation.org <efegraus@conservation.org>
2018-08-06 15:18:56 -0700

    updated this..needs much more work to be more generic. Consider removing this transform script from package

--- rscripts/DataSourceTransformScripts/Load_WI_format.R
+++ rscripts/DataSourceTransformScripts/Load_WI_format.R
@@ -9,7 +9,7 @@ library(lubridate)
 library(plyr)
 
 #Set working dir
-shinycam_path <- "/Users/efegraus/work/DataKind/MarinDownload/"
+shinycam_path <- "/Users/efegraus/work/DataKind/MarinDownload/july2018/"
 setwd(shinycam_path)
 
 ############################ 
@@ -17,7 +17,7 @@ setwd(shinycam_path)
 # Change the paths and datasets names to make this work. Data MUST be in Wildlife Insights downloadformat.
 # Provide a template
 #######
-# Marin Data Load
+# Load each Project Individually
 # Cheda Jewel
 cj_cameras <- read.csv("20-Cheda-and-Jewel-GGNRA/ChedaJewel-Cheda-and-Jewel-GGNRA-Cameras.csv")
 cj_deployments <- read.csv("20-Cheda-and-Jewel-GGNRA/ChedaJewel-Cheda-and-Jewel-GGNRA-Deployments.csv")
@@ -44,11 +44,36 @@ wh_images <-      read.csv("26-Marin-County-Parks-Cascade-Canyon-and-White-Hill/
 s_cameras <- read.csv("27-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods/MCP-North-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods-Cameras.csv")
 s_deployments <- read.csv("27-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods/MCP-North-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods-Deployments.csv")
 s_images <-      read.csv("27-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods/MCP-North-Marin-County-Parks-French-Ranch-and-Roy-s-Redwoods-Images.csv")
+
+# Beach
+b_cameras <- read.csv("28-South-MWPIP-Beach/Beach-South-MWPIP-Beach-Cameras.csv")
+b_deployments <- read.csv("28-South-MWPIP-Beach/Beach-South-MWPIP-Beach-Deployments.csv")
+b_images <-      read.csv("28-South-MWPIP-Beach/Beach-South-MWPIP-Beach-Images.csv")
+
+# East
+e_cameras <- read.csv("29-South-MWPIP-East/East-South-MWPIP-East-Cameras.csv")
+e_deployments <- read.csv("29-South-MWPIP-East/East-South-MWPIP-East-Deployments.csv")
+# Pop-off notes columns
+e_deployments2 <- subset(e_deployments,select=-c(Notes,Notes1))
+e_images <-      read.csv("29-South-MWPIP-East/East-South-MWPIP-East-Images.csv")
+
+# Tam
+t_cameras <- read.csv("30-South-MWPIP-Tam/Tam-South-MWPIP-Tam-Cameras.csv")
+t_deployments <- read.csv("30-South-MWPIP-Tam/Tam-South-MWPIP-Tam-Deployments.csv")
+# Pop-off notes columns
+t_deployments2 <- subset(t_deployments,select=-c(Notes,Notes1))
+t_images <-      read.csv("30-South-MWPIP-Tam/Tam-South-MWPIP-Tam-Images.csv")
+
+# wood
+w_cameras <- read.csv("31-South-MWPIP-Wood/Wood-South-MWPIP-Wood-Cameras.csv")
+w_deployments <- read.csv("31-South-MWPIP-Wood/Wood-South-MWPIP-Wood-Deployments.csv")
+w_images <-      read.csv("31-South-MWPIP-Wood/Wood-South-MWPIP-Wood-Images.csv")
+
 #######
 #Bind the datasetst together
-cameras <-rbind(cj_cameras, sp_cameras,wd_cameras,gg_cameras,wh_cameras,s_cameras)
-deployments <-rbind(cj_deployments, sp_deployments,wd_deployments,gg_deployments,wh_deployments,s_deployments)
-images <-rbind(cj_images, sp_images,wd_images,gg_images,wh_images,s_images)
+cameras <-rbind(cj_cameras, sp_cameras,wd_cameras,gg_cameras,wh_cameras,s_cameras,b_cameras,e_cameras,t_cameras,w_cameras)
+deployments <-rbind(cj_deployments, sp_deployments,wd_deployments,gg_deployments,wh_deployments,s_deployments,b_deployments,e_deployments2,t_deployments2,w_deployments)
+images <-rbind(cj_images, sp_images,wd_images,gg_images,wh_images,s_images,b_images,e_images,t_images,w_images)
 
 # Join them together
 cam_deploy <-inner_join(deployments,cameras,by="Camera.ID")
